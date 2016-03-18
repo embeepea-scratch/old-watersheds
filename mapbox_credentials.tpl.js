b6ec7fd7e230c950ccedd2ab4e468ecd80ede0dc
Mark Phillips <embeepea@git>
2016-03-18 00:15:03 -0400

    add README.md, mapbox_credentials.tpl.js

---
+++ mapbox_credentials.tpl.js
@@ -0,0 +1,5 @@
+// Make a copy of this file called mapbox_credentials.js, and put
+// your mapbox access token in that copy.
+mapbox_credentials = {
+        accessToken: 'PUT YOUR MAPBOX ACESSS TOKEN HERE'
+};
